63d15e94014e2c12ae3ac29d2ecc54766b20aebc
JShelley97 <84845110+JShelley97@users.noreply.github.com>
2023-10-13 12:20:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import streamlit as st 
+import webbrowser
 
 st.image("logo.png")
 st.title("Play with AI Models")
-st.text("Play with some AI models that leverage GPU compuation, all running on the below server!")
+st.write("Play with some AI models that leverage GPU compuation, all running on the below server!")
 st.button("Age Estimation",use_container_width=True)
 st.button("Pose Estimation",use_container_width=True)
 st.button("etc",use_container_width=True)
 
-st.text("More Info")
+st.write("More Info")
 col1, col2 = st.columns(2)
 with col1:
-    st.button("check out our webite!",use_container_width=True)
+    if st.button("check out our webite!",use_container_width=True):
+        webbrowser.open_new_tab("https://lac2.org")
 with col2: 
-    st.button("book an appointment with our AI Hub Manager!",use_container_width=True)
+    if st.button("book an appointment with our AI Hub Manager!",use_container_width=True):
+        webbrowser.open_new_tab("https://www.typecalendar.com/wp-content/uploads/2022/12/December-2023-Calendar.jpg")
+    
